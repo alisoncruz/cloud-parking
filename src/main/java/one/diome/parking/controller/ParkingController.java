6a1d8dfab6401fed4349a5bca6a4c5b0816621a4
alisoncruz <alisonn83@gmail.com>
2022-08-20 16:15:12 -0300

    Adicionada instância do Service

--- src/main/java/one/diome/parking/controller/ParkingController.java
+++ src/main/java/one/diome/parking/controller/ParkingController.java
@@ -1,24 +1,25 @@
 package one.diome.parking.controller;
 
 import one.diome.parking.model.Parking;
+import one.diome.parking.service.ParkingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/parkings")
 public class ParkingController {
 
+    private final ParkingService service;
+
+    private ParkingController(ParkingService service){
+        this.service = service;
+    }
+
     @GetMapping
     public List<Parking> findAll(){
-        var parking = new Parking();
-        parking.setColor("preto");
-        parking.setLicense("MSS-1111");
-        parking.setModel("vw GOL");
-        parking.setLicense("RJ");
-        return Arrays.asList(parking,parking);
+        return service.findAll();
     }
 }
