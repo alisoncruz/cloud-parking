79b3051c5b0925e79356a941c01aa876aedc256e
alisoncruz <alisonn83@gmail.com>
2022-08-20 17:40:51 -0300

    Adiciona método findById no Controller

--- src/main/java/one/diome/parking/controller/ParkingController.java
+++ src/main/java/one/diome/parking/controller/ParkingController.java
@@ -5,6 +5,7 @@ import one.diome.parking.model.Parking;
 import one.diome.parking.service.ParkingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class ParkingController {
     public ResponseEntity<List<ParkingDTO>> findAll(){
         List<ParkingDTO> list = service.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ParkingDTO> findById(@PathVariable("id") String id){
+        ParkingDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 }
