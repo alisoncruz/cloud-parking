7f1367c55503b3bd72b0f3eaec74ade9884af299
alisoncruz <alisonn83@gmail.com>
2022-08-20 22:30:47 -0300

    Adicionado Swagger

--- src/main/java/one/diome/parking/CloudParkingApplication.java
+++ src/main/java/one/diome/parking/CloudParkingApplication.java
@@ -2,8 +2,10 @@ package one.diome.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class CloudParkingApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/one/diome/parking/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package one.diome.parking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Component
+public class SwaggerConfig {
+
+    @Bean
+    public Docket getDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("one.diome.parking"))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+        return new ApiInfoBuilder()
+                .title("Parking REST API")
+                .description("Spring Boot REST API for Parking")
+                .version("1.0.0")
+                .license("Apahe License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+}
