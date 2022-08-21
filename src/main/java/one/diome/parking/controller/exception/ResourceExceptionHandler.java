871ef62d41e866ca00017667d8b11ae6228f17e2
alisoncruz <alisonn83@gmail.com>
2022-08-20 22:26:30 -0300

    Adicionada classe de exceção, handler e classe StandardError

---
+++ src/main/java/one/diome/parking/controller/exception/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package one.diome.parking.controller.exception;
+
+import one.diome.parking.service.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
+        StandardError standardError = new StandardError();
+        standardError.setTimeStamp(Instant.now());
+        standardError.setStatus(HttpStatus.NOT_FOUND.value());
+        standardError.setError("Resource not found");
+        standardError.setMessage(e.getMessage());
+        standardError.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+    }
+}
---
+++ src/main/java/one/diome/parking/service/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package one.diome.parking.service.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
