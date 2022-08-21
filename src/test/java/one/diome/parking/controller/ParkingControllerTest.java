b8dfdcd430e4c05f922a4f00e28376777c21f80c
alisoncruz <alisonn83@gmail.com>
2022-08-21 14:57:09 -0300

    Adicionada classe ParkingControllerTest

---
+++ src/test/java/one/diome/parking/controller/ParkingControllerTest.java
@@ -0,0 +1,49 @@
+package one.diome.parking.controller;
+
+import io.restassured.RestAssured;
+import one.diome.parking.controller.dto.ParkingCreateDTO;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerTest {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest() {
+        RestAssured.port = randomPort;
+    }
+
+    @Test
+    void whenFindAllThenCheckResult() {
+        RestAssured.given().when()
+                .get("/parkings")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void whenCreateThenCheckIsCreated() {
+        var createDTO = new ParkingCreateDTO();
+        createDTO.setColor("AMARELO");
+        createDTO.setLicense("WRT-5555");
+        createDTO.setModel("BRASILIA");
+        createDTO.setState("SP");
+
+        RestAssured.given().when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("parkings")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("license",Matchers.equalTo("WRT-5555"));
+
+    }
+}
