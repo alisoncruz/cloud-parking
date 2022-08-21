d29431b3904117a3f105413646e2d9ca6d309ae3
alisoncruz <alisonn83@gmail.com>
2022-08-21 20:55:01 -0300

    Injeção da interface Repository na classe de serviço

--- src/main/java/one/diome/parking/service/ParkingService.java
+++ src/main/java/one/diome/parking/service/ParkingService.java
@@ -5,67 +5,73 @@ import one.diome.parking.controller.dto.ParkingDTO;
 import one.diome.parking.controller.dto.ParkingUpdateDTO;
 import one.diome.parking.controller.mapper.ParkingMapper;
 import one.diome.parking.model.Parking;
+import one.diome.parking.repository.ParkingRepository;
 import one.diome.parking.service.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class ParkingService {
-    private static Map<String, Parking> parkingMap = new HashMap<>();
+
+    private final ParkingRepository parkingRepository;
 
     private ParkingMapper parkingMapper;
 
-    public ParkingService(ParkingMapper parkingMapper){
+    public ParkingService(ParkingMapper parkingMapper, ParkingRepository parkingRepository) {
         this.parkingMapper = parkingMapper;
+        this.parkingRepository = parkingRepository;
     }
 
-    private static String getUUID(){
-        return UUID.randomUUID().toString().replace("-","");
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
     }
 
-    public List<ParkingDTO> findAll(){
-        List<Parking> parkingList = parkingMap.values().stream().collect(Collectors.toList());
-        return parkingMapper.toParkingDTOList(parkingList);
+    public List<ParkingDTO> findAll() {
+        return parkingRepository.findAll()
+                .stream()
+                .map(parking -> parkingMapper.toParkingDTO(parking))
+                .collect(Collectors.toList());
     }
 
-    public ParkingDTO findById(String id){
-        Parking parking = parkingMap.get(id);
-        if(parking == null){
-            throw new ResourceNotFoundException("Parking with Id: "+id + " not found");
-        }
+    public ParkingDTO findById(String id) {
+        Parking parking = find(id);
         return parkingMapper.toParkingDTO(parking);
     }
 
-    public ParkingDTO create(ParkingCreateDTO dto){
+    public ParkingDTO create(ParkingCreateDTO dto) {
         Parking parking = parkingMapper.toParkingCreate(dto);
         parking.setId(getUUID());
         parking.setEntryDate(LocalDateTime.now());
-        parkingMap.put(parking.getId(),parking);
+        parking = parkingRepository.save(parking);
         return parkingMapper.toParkingDTO(parking);
     }
 
-    public void delete(String id){
-        Parking parking = parkingMap.get(id);
-        if(parking == null){
-            throw new ResourceNotFoundException("Parking with Id: "+id + " not found");
-        }
-        parkingMap.remove(id);
+    public void delete(String id) {
+        Parking parking = find(id);
+        parkingRepository.deleteById(id);
     }
 
-    public ParkingDTO update(String id, ParkingUpdateDTO dto){
-        Parking parking = parkingMap.get(id);
-        if(parking == null){
-            throw new ResourceNotFoundException("Parking with Id: "+id + " not found");
-        }
+    public ParkingDTO update(String id, ParkingUpdateDTO dto) {
+        Parking parking = find(id);
         parking.setColor(dto.getColor());
-        parkingMap.replace(id,parking);
+        parking = parkingRepository.save(parking);
         return parkingMapper.toParkingDTO(parking);
     }
 
+    private Parking find(String id) {
+        return parkingRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Parking with Id: " + id + " not found"));
+    }
+
+    public ParkingDTO checkout(String id) {
+        Parking parking = find(id);
+        parking.setExitDate(LocalDateTime.now());
+        parking.setBill(ParkingCheckout.getBill(parking));
+        parking = parkingRepository.save(parking);
+        return parkingMapper.toParkingDTO(parking);
+    }
 }
