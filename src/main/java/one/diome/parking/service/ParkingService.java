2004891a9cc1afd4243fbdc0c50de6955260a24f
alisoncruz <alisonn83@gmail.com>
2022-08-20 22:29:48 -0300

    Método para criar nova entidade, e tratamento de exceção

--- src/main/java/one/diome/parking/service/ParkingService.java
+++ src/main/java/one/diome/parking/service/ParkingService.java
@@ -1,10 +1,13 @@
 package one.diome.parking.service;
 
+import one.diome.parking.controller.dto.ParkingCreateDTO;
 import one.diome.parking.controller.dto.ParkingDTO;
 import one.diome.parking.controller.mapper.ParkingMapper;
 import one.diome.parking.model.Parking;
+import one.diome.parking.service.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,6 +45,18 @@ public class ParkingService {
 
     public ParkingDTO findById(String id){
         Parking parking = parkingMap.get(id);
+        if(parking == null){
+            throw new ResourceNotFoundException("Parking with Id: "+id + " not found");
+        }
         return parkingMapper.toParkingDTO(parking);
     }
+
+    public ParkingDTO create(ParkingCreateDTO dto){
+        Parking parking = parkingMapper.toParkingCreate(dto);
+        parking.setId(getUUID());
+        parking.setEntryDate(LocalDateTime.now());
+        parkingMap.put(parking.getId(),parking);
+        return parkingMapper.toParkingDTO(parking);
+    }
+
 }
